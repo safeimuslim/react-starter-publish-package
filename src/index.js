5042b6b9603642c887367ea43c0f41a8e898b46b
safei muslim <safeydev@gmail.com>
2017-11-14 19:10:26 +0700

    adding src

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Example extends Component {
+  render() {
+    return (
+      <div>Here!!! for custom components.</div>
+    );
+  }
+}
+export default Example;
